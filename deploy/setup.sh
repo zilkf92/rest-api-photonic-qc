a605e8ad329d656fc9671d241ae8a8d9e78cf04b
Felix Zilk <zilk.felix@gmail.com>
2021-06-02 19:24:31 +0200

    Configured for deployment

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -3,7 +3,7 @@
 set -e
 
 # TODO: Set to URL of git repo.
-PROJECT_GIT_URL='https://github.com/zilkf92/profiles-rest-api.git'
+PROJECT_GIT_URL='https://github.com/zilkf92/rest-api-photonic-qc.git'
 # Location of source code on server
 PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
 
--- deploy/update.sh
+++ deploy/update.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
+PROJECT_BASE_PATH='/usr/local/apps/rest-api-photonic-qc'
 
 git pull
 $PROJECT_BASE_PATH/env/bin/python manage.py migrate
