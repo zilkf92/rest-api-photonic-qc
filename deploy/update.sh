b718b5554c60967c6f32f51700271daf1a7519b1
Felix Zilk <zilk.felix@gmail.com>
2021-06-02 19:57:44 +0200

    configured for deployment

--- deploy/update.sh
+++ deploy/update.sh
@@ -7,6 +7,6 @@ PROJECT_BASE_PATH='/usr/local/apps/rest-api-photonic-qc'
 git pull
 $PROJECT_BASE_PATH/env/bin/python manage.py migrate
 $PROJECT_BASE_PATH/env/bin/python manage.py collectstatic --noinput
-supervisorctl restart profiles_api
+supervisorctl restart cdl_rest_api
 
 echo "DONE! :)"
