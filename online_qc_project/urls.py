e0a7cd82865c5b3b38dda46de8d41ac9b6c138a6
Felix Zilk <zilk.felix@gmail.com>
2021-06-02 19:08:10 +0200

    change project name to online_qc_project

--- online_qc_project/urls.py
+++ online_qc_project/urls.py
@@ -1,4 +1,4 @@
-"""profiles_project URL Configuration
+"""online_qc_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
