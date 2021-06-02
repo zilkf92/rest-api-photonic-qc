eb0ab39cf588af5d4825eb4c77d4a33a8ffa9df8
Felix Zilk <zilk.felix@gmail.com>
2021-06-02 19:52:01 +0200

    configured for deployment

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -5,7 +5,7 @@ set -e
 # TODO: Set to URL of git repo.
 PROJECT_GIT_URL='https://github.com/zilkf92/rest-api-photonic-qc.git'
 # Location of source code on server
-PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
+PROJECT_BASE_PATH='/usr/local/apps/rest-api-photonic-qc'
 
 echo "Installing dependencies..."
 apt-get update
@@ -29,15 +29,15 @@ $PROJECT_BASE_PATH/env/bin/python manage.py migrate
 $PROJECT_BASE_PATH/env/bin/python manage.py collectstatic --noinput
 
 # Configure supervisor
-cp $PROJECT_BASE_PATH/deploy/supervisor_profiles_api.conf /etc/supervisor/conf.d/profiles_api.conf
+cp $PROJECT_BASE_PATH/deploy/supervisor_cdl_rest_api.conf /etc/supervisor/conf.d/cdl_rest_api.conf
 supervisorctl reread
 supervisorctl update
-supervisorctl restart profiles_api
+supervisorctl restart cdl_rest_api
 
 # Configure nginx
-cp $PROJECT_BASE_PATH/deploy/nginx_profiles_api.conf /etc/nginx/sites-available/profiles_api.conf
+cp $PROJECT_BASE_PATH/deploy/nginx_cdl_rest_api.conf /etc/nginx/sites-available/cdl_rest_api.conf
 rm /etc/nginx/sites-enabled/default
-ln -s /etc/nginx/sites-available/profiles_api.conf /etc/nginx/sites-enabled/profiles_api.conf
+ln -s /etc/nginx/sites-available/cdl_rest_api.conf /etc/nginx/sites-enabled/cdl_rest_api.conf
 systemctl restart nginx.service
 
 echo "DONE! :)"
