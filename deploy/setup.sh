fcdde6f621899840a8638d5595113a643a1302f9
Felix Zilk <zilk.felix@gmail.com>
2021-06-02 19:45:48 +0200

    configured for deployment

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -9,7 +9,7 @@ PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
 
 echo "Installing dependencies..."
 apt-get update
-apt-get install -y python3-dev python3-venv sqlite python-pip supervisor nginx git
+apt-get install -y python3-dev python3-venv sqlite python3-pip supervisor nginx git
 
 # Create project directory
 mkdir -p $PROJECT_BASE_PATH
