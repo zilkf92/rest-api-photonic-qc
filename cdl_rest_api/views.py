103571f4688fbb991e39c7ae0c2a47980ee974ba
CDL-Univie <77630497+CDL-Univie@users.noreply.github.com>
2021-06-06 19:44:38 +0200

    change permission classes for Profile View Set

--- cdl_rest_api/views.py
+++ cdl_rest_api/views.py
@@ -7,7 +7,7 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 from cdl_rest_api import serializers
 from cdl_rest_api import models
@@ -270,7 +270,7 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     # Determine objects in the database which are managed in this view
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (permissions.UpdateOwnProfile, IsAuthenticated, )
+    permission_classes = (AllowAny,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
 
